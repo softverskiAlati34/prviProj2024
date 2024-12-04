297f050916295c08ca9f73a8d142ad5b4852df6e
softverskiAlati34 <softverski34@gmail.com>
2024-12-04 00:48:29 -0800

    Add files via upload

---
+++ Plavšić/Untitled-1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    printf("Unesi broj\n");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    printf("Savo Peder\n");
+return 0;
+}
---
+++ Plavšić/video.js
@@ -0,0 +1,4 @@
+var obj = {"video": {
+    "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>"
+  }}
+  document.write(obj.video.value);
